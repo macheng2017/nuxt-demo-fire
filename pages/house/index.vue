090dbe9b8fc9c20ac6c8ed47f89225acb12f72f2
unknown <gos360@163.com>
2018-04-22 23:54:32 +0800

    ++

---
+++ pages/house/index.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+.container
+  .house-media
+    .desc
+      .words {{house.words}}
+      .name {{house.name}}
+  .house-body
+    .title {{house.cname}}
+    .body {{house.intro}}
+    .title 主要角色
+    .times(v-for='(item, index) in house.swornMembers' :key='index')
+      .members
+        img(:src='item.profile')
+        .desc
+          .cname {{item.cname}}
+          .intro {{item.text}}
+.house-history(v-for='(item, index) in house.sections' :key='index')
+  .title {{item.title}}
+  .content(v-for='(item, index) in item.content' :key='index'){{text}}
+</template>
+
+<script>
+  import { mapState } from "vux";
+  export default {
+    head() {
+      return {
+        title: '家族详情'
+      }
+    },
+    computed: {
+      ...mapState({
+        house: 'showHouse'
+      })
+    },
+    beforeCreate() {
+      let id = this.$route.query.id
+
+      this.$store.dispatch('showHouse', id)
+
+    }
+  }
+</script>
