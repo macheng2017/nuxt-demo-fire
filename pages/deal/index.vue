005540b027e85c17ed1678f39c05424a29b617ae
macheng2017 <gos360@163.com>
2018-05-25 17:24:16 +0800

    微信支付接口,调用代码未完成

--- pages/deal/index.vue
+++ pages/deal/index.vue
@@ -61,7 +61,7 @@
 <script>
 import cell from '~/components/cell.vue'
 import { mapState } from 'vuex'
-import { setTimeout } from 'timers'
+import wechat from '~/static/mixins/wechat.js'
 function toggleModal(obj, content) {
   clearTimeout(obj.timer)
   obj.visible = true
@@ -106,8 +106,8 @@ export default {
   },
   computed: {
     ...mapState({
-      'imageCDN': 'imageCDN',
-      'product': 'currentProduct'
+      imageCDN: 'imageCDN',
+      product: 'currentProduct'
     })
   },
   methods: {
@@ -127,22 +127,38 @@ export default {
         address: address,
         phoneNumber: phoneNumber
       })
-      if (!res.order) {
+      // 从服务器拿到的数据
+      const data = res.data
+      if (!data || !data.success) {
         // 使用提示窗口
         toggleModal(this.modal, '服务器异常,请等待后重新尝试')
         return
       }
       // 微信支付api
-      window.wx.chooseWXPay({
-        //
+      // 微信公众平台  https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421141115
+      wx.chooseWXPay({
+        timestamp: data.timestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+        nonceStr: data.nonceStr, // 支付签名随机串，不长于 32 位
+        package: data.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）
+        signType: data.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+        paySign: data.paySign, // 支付签名
+        success: response => {
+          // 支付成功后的回调函数
+          try {
+            window.WeixinJSBridge.log(response.err_msg)
+          }
+        }
       })
-      // console.log(item)
-      // 支付功能暂未实现
     }
   },
+  // https://cn.vuejs.org/v2/guide/mixins.html
+  mixins: [wechat],
   async beforeMount() {
     const id = this.$route.query.id
     this.$store.dispatch('showProduct', id)
+    // 在获取商品信息之后,初始化微信网页端接口行为
+    const url = window.location.href
+    await this.wechatConfig(url)
   },
   components: {
     cell
---
+++ static/mixins/wechat.js
@@ -0,0 +1,39 @@
+// mixins 是一种分发vue组件中可复用功能的非常灵活的方式
+export default {
+  methods: {
+    // 让微信客户端知道,在当前的url地址网页能有调用微信api的能力
+    async wechatConfig(url) {
+      // 通过服务器拿到签名
+      const res = await this.$store.dispatch('getWechatSignature', url)
+      const {
+        data,
+        success
+      } = res.data
+      if (!success) throw new Error('不能成功获取服务器签名!')
+      // 拿到全局暴露的微信sdk对象
+      const wx = window.wx
+      // 初始化微信sdk对象, 可以参考文档
+      // https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421141115
+
+      wx.config({
+        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        appId: data.appId, // 必填，公众号的唯一标识
+        timestamp: data.timestamp, // 必填，生成签名的时间戳
+        nonceStr: data.nonceStr, // 必填，生成签名的随机串
+        signature: data.signature, // 必填，签名
+        jsApiList: ['previewImage',
+          'hideAllNonBaseMenuItem',
+          'showAllNonBaseMenuItem',
+          'onMenuShareTimeline',
+          'onMenuShareAppMessage',
+          'chooseWXPay'
+        ] // 必填，需要使用的JS接口列表
+      })
+
+      wx.ready(() => {
+        // this.wechatSetMenu() // inin 微信按钮
+        // this.wechatShare()
+      })
+    }
+  }
+}
